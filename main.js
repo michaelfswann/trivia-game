74bd79f6a6e488ad8ca437badd625e0fb95020f3
michaelfswann <mikeswann@mail.com>
2020-10-04 22:21:01 +0100

    built function for true or false buttons that check answer

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let data, questionNumber, questionString, answer;
 console.log('Hello');
 
+const divButtonNewGame = document.getElementById("new-game-button");
+const buttonsTrueFalse = document.getElementById("true-false-buttons");
+const questionResults = document.getElementById("result");
+const questionBeingDisplayed = document.querySelector("#question")
+
+
 
 // async fetch function
 async function getTrivia () {
@@ -24,33 +30,65 @@ questionNumber = 0;
 
 function displayQuestion () {
     questionString = data.results[questionNumber].question;
-    let questionBeingDisplayed = document.querySelector("#question")
+    console.log(questionString);
     questionBeingDisplayed.innerText = questionString;
-    answer = data.results[questionNumber].correctAnswer;
+    answer = data.results[questionNumber].correct_answer;
+    console.log(answer)
 }
 
 function clickTrue() {
+    buttonsTrueFalse.style.display = "none";
+    questionResults.style.display = "block";
+    if (answer === 'True') {
+        questionResults.innerText = 'Correct!'
+    } else {
+        questionResults.innerText = 'Wrong Answer!'
+    }
+    divButtonNewGame.style.display = "block";
+    questionNumber++
 
 }
 
+function clickFalse() {
+    buttonsTrueFalse.style.display = "none";
+    questionResults.style.display = "block";
+    if (answer === 'False') {
+        questionResults.innerText = 'Correct!'
+    } else {
+        questionResults.innerText = 'Wrong Answer!'
+    }
+    divButtonNewGame.style.display = "block";
+    questionNumber++
+
+}
 
 
 
 function showButton() {
-    let divButtonNewGame = document.getElementById("new-game-button");
+    
     divButtonNewGame.style.display = "none";
-    let buttonsTrueFalse = document.getElementById("true-false-buttons");
+    
     buttonsTrueFalse.style.display = "block";
 };
 
 function newGame() {
-
+    questionResults.style.display = "none";
     displayQuestion();
+    console.log('being called')
     showButton()
 };
 
-let newGameButton = document.querySelector("#new-game");
+const newGameButton = document.querySelector("#new-game");
 
 
 newGameButton.addEventListener('click', newGame);
  
+
+const trueButton = document.getElementById('true-button');
+
+trueButton.addEventListener('click', clickTrue);
+
+
+const falseButton = document.getElementById('false-button');
+
+falseButton.addEventListener('click', clickFalse);
